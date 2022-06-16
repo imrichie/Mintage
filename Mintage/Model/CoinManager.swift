5d40d216ae275f3cdf555f09f41706217e9ad774
Richie Flores <rfloresc@icloud.com>
2022-06-16 12:23:31 -0700

    Refactored the constructing of the URL with the use of URL Components

--- Mintage/Model/CoinManager.swift
+++ Mintage/Model/CoinManager.swift
@@ -10,11 +10,14 @@ import Foundation
 struct CoinManager {
   let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
   
-  func getCryptoPrice(in currency: String) {
-    let baseURL = "https://rest.coinapi.io"
-    let endpoint = "/v1/exchangerate/BTC/\(currency)/?apikey=\(Constants.coinApiKey)"
+  func getPrices(in currency: String) {
+    var components = URLComponents()
+    components.scheme = "https"
+    components.host = "rest.coinapi.io"
+    components.path = "/v1/exchangerate/BTC/\(currency)"
+    components.queryItems = [URLQueryItem(name: "apikey", value: Constants.coinApiKey)]
     
-    if let formattedURL = URL(string: baseURL + endpoint) {
+    if let formattedURL = components.url {
       performRequest(with: formattedURL)
     }
   }
